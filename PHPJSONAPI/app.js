17a7d52c0cc25469ab3e90f1192c3484075e493b
Tyler Truitt <tylertruitt12@outlook.com>
2017-07-01 18:57:30 -0500

    first commit

---
+++ PHPJSONAPI/app.js
@@ -0,0 +1,11 @@
+﻿function getWeatherForecast() {
+    var forecast = jQuery(".forecast")
+    var zipCode = jQuery(".zipCode").val()
+    //call back function to present said JSON data to the user.
+    //PHP code is stored on localhost:8080 and serves as the processing backend
+    jQuery.getJSON("http://arctic-leaf-15909.herokuapp.com/forecast/" + zipCode, function (data) {
+        console.log(data)
+        forecast.html("Today's Temp: " + data.main.temp + " &#x2109;<br>Humidity: "
+            + data.main.humidity + " %<br>Pressure: " + data.main.pressure + " Pa")
+    })
+}
